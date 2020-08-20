4fbfb448e35ad66755175cdd9d2a67b0b169e89a
DeAundre Prescott <deaundre.prescott@codeimmersives.com>
2020-08-20 12:51:32 -0400

    done

--- main.js
+++ main.js
@@ -1,3 +1,67 @@
+const makeRunOn = function(str){
+  return sent = str.split('. ').join(', ')  
+}
+
+const totalScore = function(arr){
+let newScore = 0;
+for(const item of arr){
+  if(item.multiplier === undefined){
+    item.multiplier = 1
+  }
+  newScore += item.score * item.multiplier;
+  }
+return newScore
+}
+
+const nightOwls = function(people){
+  return people.filter(function(person){
+    return person.localTime>=100 && person.localTime<=400 && person.asleep ===false
+  })
+
+}
+
+const getToBed = function(people){
+  return people.map(function(person){
+    let newPerson = {...person}
+    if (person.localTime>=100 && person.localTime<=400 && person.asleep ===false){
+      newPerson.asleep = true
+    }
+    return newPerson
+  })
+}
+
+const findIndices = function(arr, callBack){
+let result = []
+for (let i = 0; i < arr.length; i++){
+  if(callBack(arr[i])){
+    result.push(i)
+  }
+}
+return result
+}
+
+const Faqtory = function(){
+return {
+  questions: [],
+  length: 0,
+  addQuestion: function(text){
+    this.questions.push(
+      {
+      text:text,
+      id:this.length,
+      answered:false,
+      }
+    )
+      this.length++
+    },
+    answerQuestion: function(id,answer){
+      if(this.questions[id].answered === false){
+        this.questions[id].answer = answer
+        this.questions[id].answered = true
+      }
+    },
+  }
+}
 
 
 
